07cf0a69b309bf468b522cbb0e1f3b2346c7ae0b
Clive Walkden <clive@sozodesign.co.uk>
2022-10-13 09:00:51 +0100

    Updated to attempt to delete

---
+++ README.md
@@ -0,0 +1,6 @@
+# Cleanup old files on WasabiSys
+
+This executable takes a configuration file and cleans up old files outside the given timeframes.
+
+## Run
+`go run cmd/main.go`
--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"log"
 	"time"
 	"wasabiCleanup/internal/client/wasabi"
@@ -12,10 +13,12 @@ import (
 type S3Object struct {
 	Key          string
 	LastModified time.Time
+	Size         int64
 }
 
 type S3Objects struct {
-	Items []S3Object
+	Items []types.ObjectIdentifier
+	Size  int64
 }
 
 var retention map[string]int
@@ -32,7 +35,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println("First page results:")
+	log.Println("Working...")
 	for _, object := range buckets.Buckets {
 		if retention[*object.Name] == 0 {
 			continue
@@ -40,39 +43,86 @@ func main() {
 
 		// Return files that need deleting from this bucket based on the Retention Policy
 		objectList := S3Objects{}
+		safeList := S3Objects{}
+		maxKeys := 0
 
-		result, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{Bucket: object.Name})
-		if err != nil {
-			log.Fatal(err)
-		}
+		params := &s3.ListObjectsV2Input{Bucket: object.Name}
+
+		// Create the Paginator for the ListObjectsV2 operation.
+		p := s3.NewListObjectsV2Paginator(client, params, func(o *s3.ListObjectsV2PaginatorOptions) {
+			if v := int32(maxKeys); v != 0 {
+				o.Limit = v
+			}
+		})
 
 		// The date we need to delete items prior to
-		comparisonDate := time.Now().AddDate(0, 0, -retention[*object.Name]+1)
+		comparisonDate := time.Now().AddDate(0, 0, -retention[*object.Name]-1)
 
-		for _, object := range result.Contents {
-			//fmt.Printf("Object Name: %s Object Modifiled Date: %s\n", *object.Key, object.LastModified)
+		// Iterate through the S3 object pages, printing each object returned.
+		var i int
+		for p.HasMorePages() {
+			i++
 
-			if object.LastModified.Before(comparisonDate) {
-				objectList.Items = append(objectList.Items, S3Object{
-					Key:          *object.Key,
-					LastModified: *object.LastModified,
-				})
-				fmt.Printf("Object Name: %s Object Modifiled Date: %s\n", *object.Key, object.LastModified)
+			// Next Page takes a new context for each page retrieval. This is where
+			// you could add timeouts or deadlines.
+			page, err := p.NextPage(context.TODO())
+			if err != nil {
+				log.Fatalf("failed to get page %v, %v", i, err)
+			}
+
+			// Log the objects found
+			for _, obj := range page.Contents {
+				if obj.LastModified.Before(comparisonDate) {
+					objectList.Items = append(objectList.Items, types.ObjectIdentifier{
+						Key: obj.Key,
+					})
+					objectList.Size += obj.Size
+					//fmt.Printf("Object Name: %s Object Modified Date: %s\n", *obj.Key, obj.LastModified)
+				} else {
+					safeList.Items = append(safeList.Items, types.ObjectIdentifier{
+						Key: obj.Key,
+					})
+					safeList.Size += obj.Size
+				}
 			}
 		}
 
-		fmt.Printf("Bucket Name: %s Retention Policy: %d\n", *object.Name, retention[*object.Name])
-	}
+		// Delete the objects that match
+		fmt.Printf("Deleting %d objects totallying %s from the %s Bucket\n", len(objectList.Items), ByteCountSI(objectList.Size), *object.Name)
+		fmt.Printf("Remaining %d objects totallying %s in the %s Bucket\n", len(safeList.Items), ByteCountSI(safeList.Size), *object.Name)
+		//bar := progressbar.Default(int64(len(objectList.Items)))
 
-	//output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-	//	Bucket: aws.String("sozo-db-backups"),
-	//})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//log.Println("First page results:")
-	//for _, object := range output.Contents {
-	//	log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
-	//}
+		//for x := 0; x < len(objectList.Items); x++ {
+		del := &types.Delete{
+			Objects: objectList.Items,
+			Quiet:   true,
+		}
+		//fmt.Println(del)
+		input := s3.DeleteObjectsInput{
+			Bucket: object.Name,
+			Delete: del,
+		}
+		//fmt.Println(input)
+		output, err := client.DeleteObjects(context.Background(), &input)
+		if err != nil {
+			panic(err)
+		}
+		//bar.Add(1)
+		//}
+		fmt.Println(output)
+	}
+}
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
 }
